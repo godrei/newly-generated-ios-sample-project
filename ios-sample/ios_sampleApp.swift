8538abec840710a83f7fe211936a220dc5965f98
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2023-08-01 15:53:38 +0200

    Initial Commit

---
+++ ios-sample/ios_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_sampleApp.swift
+//  ios-sample
+//
+//  Created by Krisztián Gödrei on 2023. 08. 01..
+//
+
+import SwiftUI
+
+@main
+struct ios_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
